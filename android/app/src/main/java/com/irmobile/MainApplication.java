cb91a951658c76f996c0b6c51a43247ce19496be
Dave Sag <davesag@gmail.com>
2019-07-03 16:26:54 +0530

    #16 updated dependencies and improved spash screen

--- android/app/src/main/java/com/irmobile/MainApplication.java
+++ android/app/src/main/java/com/irmobile/MainApplication.java
@@ -3,10 +3,10 @@ package com.irmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.tradle.react.UdpSocketsModule;
 import com.peel.react.TcpSocketsModule;
 import com.peel.react.rnos.RNOSModule;
-import com.bitgo.randombytes.RandomBytesPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -32,10 +32,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new UdpSocketsModule(),
             new TcpSocketsModule(),
             new RNOSModule(),
-            new RandomBytesPackage(),
             new VectorIconsPackage(),
             new RNScreensPackage(),
             new RNGestureHandlerPackage(),
--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,14 @@
  */
 
 module.exports = {
+  resolver: {
+    extraNodeModules: {
+      // Polyfills for node libraries
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      vm: require.resolve('vm-browserify')
+    }
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
