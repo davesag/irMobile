d14aa4e96d5b9bfcaaa6448b52d0fcba594f727a
Dave Sag <davesag@gmail.com>
2019-07-08 08:06:17 +0530

    n/a use the new ir-api and set timeout to 5000

--- app/services/ir/api/index.js
+++ app/services/ir/api/index.js
@@ -5,7 +5,7 @@ import getBalances from './getBalances'
 const getApi = ({ apiKey, apiSecret } = {}) => {
   if (!apiKey || !apiSecret) throw new Error('Missing API Credentials')
 
-  const api = ir(apiKey, apiSecret, { timeout: 100 })
+  const api = ir(apiKey, apiSecret, { timeout: 5000 })
 
   return {
     getBalances: getBalances(api)
