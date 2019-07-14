87afa1eceae30167cc97cc37e5976c209a6fa408
Dave Sag <davesag@gmail.com>
2019-07-14 16:57:19 +0600

    #37 improved spacing

--- app/components/APIDetailsForm/styles.js
+++ app/components/APIDetailsForm/styles.js
@@ -6,9 +6,9 @@ export default StyleSheet.create({
     fontSize: 7,
     fontFamily: Platform.select({ android: 'monospace', ios: 'Courier New' })
   },
-  buttons: { flexDirection: 'row', justifyContent: 'flex-end' },
-  saveButton: { marginRight: 10 },
-  clearButton: { marginLeft: 10, marginRight: 10 },
+  buttons: { marginTop: 10, flexDirection: 'row', justifyContent: 'flex-end' },
+  saveButton: { marginRight: 5 },
+  clearButton: { marginLeft: 5, marginRight: 10 },
   error: { color: 'red' },
   switchWrapper: { flexDirection: 'row', alignItems: 'center' },
   switchLabel: { marginLeft: 10 }
